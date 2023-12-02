da807431a31c11cea564632c0afb6fb9ff5c4827
okvignesh <ok.vignesh@outlook.com>
2023-12-02 21:44:16 +0000

    removed fb

--- src/containers/LoginScreen/index.js
+++ src/containers/LoginScreen/index.js
@@ -7,7 +7,6 @@ import {
   StyleSheet,
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
-import {LoginButton, AccessToken, LoginManager} from 'react-native-fbsdk-next';
 
 const LoginScreen = props => {
   const [email, setEmail] = useState('');
@@ -34,33 +33,6 @@ const LoginScreen = props => {
   const navigateToSignup = () => {
     props.navigation.navigate('SignupScreen');
   };
-
-  // async function onFacebookButtonPress() {
-  //   // Attempt login with permissions
-  //   const result = await LoginManager.logInWithPermissions([
-  //     'public_profile',
-  //     'email',
-  //   ]);
-
-  //   if (result.isCancelled) {
-  //     throw 'User cancelled the login process';
-  //   }
-
-  //   // Once signed in, get the users AccessToken
-  //   const data = await AccessToken.getCurrentAccessToken();
-
-  //   if (!data) {
-  //     throw 'Something went wrong obtaining access token';
-  //   }
-
-  //   // Create a Firebase credential with the AccessToken
-  //   const facebookCredential = auth.FacebookAuthProvider.credential(
-  //     data.accessToken,
-  //   );
-
-  //   // Sign-in the user with the credential
-  //   return auth().signInWithCredential(facebookCredential);
-  // }
 
   return (
     <View style={styles.container}>
@@ -91,48 +63,6 @@ const LoginScreen = props => {
           Don't have an account? Sign up here
         </Text>
       </TouchableOpacity>
-
-      {/* <LoginButton
-        onLoginFinished={(error, result) => {
-          // if (error) {
-          //   console.log('login has error: ' + result.error);
-          // } else if (result.isCancelled) {
-          //   console.log('login is cancelled.');
-          // } else {
-          //   AccessToken.getCurrentAccessToken().then(data => {
-          //     // console.log(data?.accessToken.toString());
-
-          //     const facebookCredential = auth.FacebookAuthProvider.credential(
-          //       data.accessToken,
-          //     );
-
-          //     // Sign-in the user with the credential
-          //     auth()
-          //       .signInWithCredential(facebookCredential)
-          //       .then(success => {
-          //         console.log('Is it success really ? ', success);
-          //         props.navigation.navigate('ProfileScreen');
-          //       })
-          //       .catch(error => {
-          //         console.log('Is it error really ? ', error);
-          //       });
-          //   });
-          // }
-          
-          // onFacebookButtonPress;
-
-          // if (error) {
-          //   console.log('login has error: ' + result.error);
-          // } else if (result.isCancelled) {
-          //   console.log('login is cancelled.');
-          // } else {
-          //   AccessToken.getCurrentAccessToken().then(data => {
-          //     console.log(data.accessToken.toString());
-          //   });
-          // }
-        }}
-        onLogoutFinished={() => console.log('logout.')}
-      /> */}
     </View>
   );
 };
