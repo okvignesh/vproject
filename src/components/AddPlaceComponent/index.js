9b2d60111f1ad35d3ec966d582464aec02bba1a9
okvignesh <ok.vignesh@outlook.com>
2023-12-04 01:30:35 +0000

    My Places and Add Place done using simple maps

--- src/components/AddPlaceComponent/index.js
+++ src/components/AddPlaceComponent/index.js
@@ -1,32 +1,93 @@
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Alert,
+} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
 const AddPlace = props => {
+  const [region, setRegion] = useState({
+    latitude: 51.4657689,
+    longitude: -2.5722472,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  const [locationName, setLocationName] = useState('');
+  const currentUser = auth().currentUser;
+  // console.log('Current User ', currentUser); //displayName //email //uid
+
+  const handleRegionChangeComplete = newRegion => {
+    setRegion(newRegion);
+  };
+
+  const handleAddPlace = async () => {
+    try {
+      await firestore().collection('UserMyPlaces').add({
+        userId: currentUser.uid,
+        userName: currentUser.displayName,
+        latitude: region.latitude,
+        longitude: region.longitude,
+        placeName: locationName,
+        author: 'Vignesh',
+      });
+
+      console.log('Place added successfully!');
+      Alert.alert('Data Added Successfully');
+    } catch (error) {
+      console.error('Error adding place:', error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Add Place Component</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Add Place Component</Text>
+      <MapView
+        style={styles.map}
+        region={region}
+        onRegionChangeComplete={handleRegionChangeComplete}>
+        <Marker
+          coordinate={{latitude: region.latitude, longitude: region.longitude}}
+        />
+      </MapView>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter location name"
+        value={locationName}
+        onChangeText={setLocationName}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleAddPlace}>
+        <Text style={styles.buttonText}>Add Place</Text>
+      </TouchableOpacity>
       <TouchableOpacity
+        style={styles.button}
         onPress={() => {
           props.navigation.navigate('ProfileScreen');
         }}>
-        <Text>Go to Dashboard</Text>
+        <Text style={styles.buttonText}>Go to Dashboard</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
-export default AddPlace;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     padding: 16,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  map: {
+    flex: 1,
+    height: 300,
     marginBottom: 16,
   },
   input: {
@@ -50,3 +111,5 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+
+export default AddPlace;
