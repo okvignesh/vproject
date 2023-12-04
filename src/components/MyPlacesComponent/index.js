22ff523e0479207f9b96c7139ccfa2dabedc88ee
okvignesh <ok.vignesh@outlook.com>
2023-12-04 01:30:58 +0000

    My Places

--- src/components/MyPlacesComponent/index.js
+++ src/components/MyPlacesComponent/index.js
@@ -1,15 +1,66 @@
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {View, Text, TouchableOpacity, FlatList, StyleSheet} from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
 
-const MyPlaces = props => {
+const MyPlaces = ({navigation}) => {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const currentUser = auth().currentUser;
+        console.log('Current User ', currentUser);
+
+        if (currentUser) {
+          const userUid = currentUser.uid;
+
+          const querySnapshot = await firestore()
+            .collection('UserMyPlaces')
+            .where('userId', '==', userUid)
+            .get();
+
+          const data = querySnapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+
+          setPlaces(data);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const renderPlaceItem = ({item}) => (
+    <View style={styles.placeItem}>
+      <Text style={styles.placeName}>{item.placeName}</Text>
+      <Text style={styles.author}>{`Author: ${item.author}`}</Text>
+      <Text style={styles.userName}>{`User: ${item.userName}`}</Text>
+      <Text
+        style={
+          styles.coordinates
+        }>{`Coordinates: ${item.latitude}, ${item.longitude}`}</Text>
+    </View>
+  );
+
   return (
-    <View>
-      <Text>MyPlaces Component</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>All Places Component</Text>
+      <FlatList
+        data={places}
+        keyExtractor={item => item.id}
+        renderItem={renderPlaceItem}
+      />
       <TouchableOpacity
+        style={styles.button}
         onPress={() => {
-          props.navigation.navigate('ProfileScreen');
+          navigation.navigate('ProfileScreen');
         }}>
-        <Text>Go to Dashboard</Text>
+        <Text style={styles.buttonText}>Go to Dashboard</Text>
       </TouchableOpacity>
     </View>
   );
@@ -17,4 +68,48 @@ const MyPlaces = props => {
 
 export default MyPlaces;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  placeItem: {
+    backgroundColor: '#f5f5f5',
+    padding: 16,
+    marginBottom: 16,
+    borderRadius: 8,
+  },
+  placeName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  author: {
+    fontSize: 14,
+    color: '#555',
+  },
+  userName: {
+    fontSize: 14,
+    color: '#555',
+  },
+  coordinates: {
+    fontSize: 14,
+    color: '#555',
+  },
+  button: {
+    backgroundColor: '#3498db',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 16,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
