40ac2287515f63579adf83980f3c22979802dde4
okvignesh <ok.vignesh@outlook.com>
2023-12-18 14:18:30 +0000

    one to one chat

--- src/containers/AllUsersPosition/index.js
+++ src/containers/AllUsersPosition/index.js
@@ -1,3 +1,173 @@
+// import React, {useState, useEffect} from 'react';
+// import {
+//   View,
+//   StyleSheet,
+//   Text,
+//   TouchableOpacity,
+//   FlatList,
+//   Dimensions,
+// } from 'react-native';
+// import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+// import firestore from '@react-native-firebase/firestore';
+// import {useNavigation} from '@react-navigation/native';
+
+// const windowWidth = Dimensions.get('window').width;
+
+// const AllUsersPosition = () => {
+//   const [userPositions, setUserPositions] = useState([]);
+//   const [mapView, setMapView] = useState(null);
+//   const [selectedUser, setSelectedUser] = useState(null);
+//   const navigation = useNavigation();
+
+//   useEffect(() => {
+//     const unsubscribe = firestore()
+//       .collection('UsersPosition')
+//       .onSnapshot(snapshot => {
+//         const positions = snapshot.docs.map(doc => ({
+//           id: doc.id,
+//           ...doc.data(),
+//         }));
+//         setUserPositions(positions);
+//       });
+
+//     return () => unsubscribe();
+//   }, []);
+
+//   const handleTrackUser = userName => {
+//     const selectedUserPosition = userPositions.find(
+//       user => user.userName === userName,
+//     );
+
+//     if (selectedUserPosition) {
+//       mapView.animateToRegion({
+//         latitude: selectedUserPosition.currentLatitude,
+//         longitude: selectedUserPosition.currentLongitude,
+//         latitudeDelta: 0.02,
+//         longitudeDelta: 0.02,
+//       });
+
+//       setSelectedUser(userName);
+//     }
+//   };
+
+//   const handleChat = userName => {
+//     navigation.navigate('PubnubScreen', {userName});
+//   };
+
+//   const renderItem = ({item}) => (
+//     <TouchableOpacity
+//       style={[
+//         styles.userListItem,
+//         item.userName === selectedUser && styles.selectedUser,
+//       ]}>
+//       <Text style={styles.userName}>{item.userName}</Text>
+//       <TouchableOpacity
+//         style={styles.trackButton}
+//         onPress={() => handleTrackUser(item.userName)}>
+//         <Text style={styles.trackButtonText}>Track</Text>
+//       </TouchableOpacity>
+//       <TouchableOpacity
+//         style={styles.chatButton}
+//         onPress={() => handleChat(item.userName)}>
+//         <Text style={styles.chatButtonText}>Chat</Text>
+//       </TouchableOpacity>
+//     </TouchableOpacity>
+//   );
+
+//   return (
+//     <View style={styles.container}>
+//       <View style={{flex: 2}}>
+//         <MapView
+//           provider={PROVIDER_GOOGLE}
+//           style={styles.map}
+//           ref={ref => setMapView(ref)}
+//           initialRegion={{
+//             latitude: 51.4657689,
+//             longitude: -2.5722472,
+//             latitudeDelta: 0.0922,
+//             longitudeDelta: 0.0421,
+//           }}>
+//           {userPositions.map(user => (
+//             <Marker
+//               key={user.id}
+//               coordinate={{
+//                 latitude: user.currentLatitude,
+//                 longitude: user.currentLongitude,
+//               }}
+//               title={`User: ${user.userName}`}
+//               description={`Speed: ${user.speed}, Time: ${user.locationTime}`}
+//             />
+//           ))}
+//         </MapView>
+//       </View>
+//       <View style={{flex: 1}}>
+//         <FlatList
+//           data={userPositions}
+//           renderItem={renderItem}
+//           keyExtractor={item => item.id}
+//           style={styles.userList}
+//         />
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     flexDirection: 'row',
+//     justifyContent: 'center',
+//     alignItems: 'stretch',
+//     padding: 16,
+//   },
+//   map: {
+//     flex: 1,
+//     width: windowWidth * 0.7, // Adjust the width based on your preference
+//     aspectRatio: 1,
+//   },
+//   userList: {
+//     margin: 10,
+//   },
+//   userListItem: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     padding: 8,
+//     borderRadius: 8,
+//     backgroundColor: '#3498db',
+//     marginVertical: 4,
+//   },
+//   selectedUser: {
+//     backgroundColor: '#27ae60',
+//   },
+//   userName: {
+//     color: 'white',
+//     fontWeight: 'bold',
+//   },
+//   trackButton: {
+//     backgroundColor: '#e67e22',
+//     padding: 8,
+//     borderRadius: 8,
+//     marginLeft: 8,
+//   },
+//   trackButtonText: {
+//     color: 'white',
+//     fontWeight: 'bold',
+//   },
+//   chatButton: {
+//     backgroundColor: '#2ecc71',
+//     padding: 8,
+//     borderRadius: 8,
+//     marginLeft: 8,
+//   },
+//   chatButtonText: {
+//     color: 'white',
+//     fontWeight: 'bold',
+//   },
+// });
+
+// export default AllUsersPosition;
+
 import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Text, TouchableOpacity, FlatList} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
@@ -53,7 +223,7 @@ const AllUsersPosition = () => {
       ]}
       onPress={() => handleTrackUser(item.userName)}>
       <Text style={styles.userName}>{item.userName}</Text>
-      <TouchableOpacity onPress={() => handleChat(item.userName)}>
+      <TouchableOpacity onPress={() => handleChat(item.userId)}>
         <Text style={styles.chatButton}>Chat</Text>
       </TouchableOpacity>
     </TouchableOpacity>
--- src/containers/PubnubScreen/index.js
+++ src/containers/PubnubScreen/index.js
@@ -1,99 +1,181 @@
+// PubNubScreen.js
 import React, {useEffect, useState} from 'react';
 import PubNub from 'pubnub';
 import {PubNubProvider, usePubNub} from 'pubnub-react';
 import {Text, View, TouchableOpacity, TextInput, FlatList} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const pubnub = new PubNub({
   publishKey: 'pub-c-f2919219-ac20-4403-b537-a678b79b4381',
   subscribeKey: 'sub-c-c5ddc634-c6fc-11e7-afd4-56ea5891403c',
-  uuid: 'sdkjsdfgsjkhfgskjfgk',
+  uuid: auth().currentUser ? auth().currentUser.uid : 'sdkjsdfgsjkhfgskjfgk',
 });
 
-const PubNubScreen = ({route}) => {
+// const userId1 = auth().currentUser.uid;
+
+const PubNubScreen = ({route, navigation}) => {
   const {userName} = route.params;
+  const [chatEnded, setChatEnded] = useState(false);
+
+  useEffect(() => {
+    setChatEnded(false);
+  }, []);
+
+  useEffect(() => {
+    setChatEnded(false);
+  }, [route]);
+
+  const handleEndChat = () => {
+    // Add logic to end the chat, navigate back to AllUsersPosition screen, and notify the other user
+    setChatEnded(true);
+    navigation.navigate('AllUsersPosition'); // Navigate to the AllUsersPosition screen
+  };
+
   return (
     <PubNubProvider client={pubnub}>
-      <Chat userName={userName} />
+      <Chat
+        userName={userName}
+        chatEnded={chatEnded}
+        onEndChat={handleEndChat}
+      />
     </PubNubProvider>
   );
 };
 
-function Chat({userName}) {
+function Chat({userName, chatEnded, onEndChat}) {
   const pubnub = usePubNub();
-  const [channels] = useState(['ITC']);
-  const [messages, addMessage] = useState([]);
+  function createUniqueChannelName(userId1, userId2) {
+    // Sort user IDs alphabetically
+    const sortedUserIds = [userId1, userId2].sort();
+
+    console.log(sortedUserIds);
+
+    // Use a separator (you can choose any character that suits your needs)
+    const separator = '_';
+
+    // Concatenate sorted user IDs with the separator
+    const concatenatedIds = sortedUserIds.join(separator);
+
+    console.log(concatenatedIds);
+
+    // Return the concatenated string as the unique channel name
+    return concatenatedIds;
+  }
+
+  const uniqueChannelName = createUniqueChannelName(
+    auth().currentUser.uid,
+    userName,
+  );
+  //   const [channels] = useState([userName]);
+  // abc1 -> user id 1
+  // def2 -> user id 2
+  // abcdef12 like this?
+  const [channels] = useState([uniqueChannelName]); //ITC
+  const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState('');
 
-  const handleMessage = event => {
-    const message = event.message;
+  useEffect(() => {
+    const handleMessage = event => {
+      const message = event.message;
+      console.log('Received message', message);
 
-    if (typeof message === 'string' || message.hasOwnProperty('text')) {
-      const text = message.text || message;
-      addMessage(messages => [...messages, text]);
-    }
-  };
+      if (typeof message === 'string' || message.hasOwnProperty('text')) {
+        const text = message.text || message;
+        setMessages(prevMessages => [...prevMessages, text]);
+      }
+    };
+
+    pubnub.addListener({message: handleMessage});
+    pubnub.subscribe({channels});
+
+    return () => {
+      pubnub.removeListener({message: handleMessage});
+      pubnub.unsubscribe({channels});
+    };
+  }, [pubnub, channels]);
 
   const sendMessage = message => {
     if (message) {
-      pubnub
-        .publish({channel: channels[0], message})
-        .then(() => setMessage(''));
+      pubnub.publish({
+        channel: channels[0],
+        message,
+        // storeInHistory: true, // should be true to use ttl
+        // ttl: 1, // Messages will be expired in 1 hour
+      });
+      setMessage('');
     }
   };
 
-  useEffect(() => {
-    pubnub.addListener({message: handleMessage});
-    pubnub.subscribe({channels});
-  }, [pubnub, channels]);
+  const endChat = () => {
+    onEndChat();
+    pubnub.unsubscribeAll();
+  };
 
   return (
-    <View>
-      <TextInput
-        autoComplete="off"
-        autoCorrect={false}
-        value={message}
-        onChangeText={changedText => {
-          setMessage(changedText);
-        }}
-        placeholder="write message"
-        style={{
-          margin: 10,
-          backgroundColor: 'grey',
-          color: 'white',
-          height: 40,
-          padding: 5,
-        }}
-      />
-
-      <TouchableOpacity
-        style={{
-          margin: 10,
-          backgroundColor: 'blue',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: 44,
-        }}
-        onPress={() => {
-          sendMessage(message);
-        }}>
-        <Text
-          style={{
-            color: 'white',
-          }}>
-          Send Message
-        </Text>
-      </TouchableOpacity>
+    <View style={{flex: 1, padding: 16}}>
+      <Text style={{fontSize: 20, fontWeight: 'bold', marginBottom: 10}}>
+        {`Chat with ${userName}`}
+      </Text>
 
       <FlatList
         data={messages}
-        renderItem={({item, index}) => {
-          return (
-            <View>
-              <Text>{item}</Text>
-            </View>
-          );
-        }}
+        renderItem={({item, index}) => (
+          <View style={{marginVertical: 5}}>
+            <Text>{item}</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
       />
+
+      {!chatEnded && (
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <TextInput
+            autoComplete="off"
+            autoCorrect={false}
+            value={message}
+            onChangeText={changedText => {
+              setMessage(changedText);
+            }}
+            placeholder="Write a message"
+            style={{
+              flex: 1,
+              backgroundColor: 'lightgrey',
+              color: 'black',
+              height: 40,
+              paddingHorizontal: 10,
+              borderRadius: 8,
+            }}
+          />
+
+          <TouchableOpacity
+            style={{
+              marginLeft: 10,
+              backgroundColor: 'blue',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 44,
+              borderRadius: 8,
+            }}
+            onPress={() => sendMessage(message)}>
+            <Text style={{color: 'white'}}>Send</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      {/* {!chatEnded && (
+        <TouchableOpacity
+          style={{
+            marginTop: 10,
+            backgroundColor: 'red',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 44,
+            borderRadius: 8,
+          }}
+          onPress={endChat}>
+          <Text style={{color: 'white'}}>End Chat</Text>
+        </TouchableOpacity>
+      )} */}
     </View>
   );
 }
